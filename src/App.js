e3a231b16edf10e79ad90552cd15427f6fade2eb
github username <github email address>
2019-04-21 10:07:33 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,23 @@ import Authors from './components/Authors';
 import BookInput from './components/BookInput';
 import Books from './components/Books';
 
-
 export class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <h2>Authors</h2>
-          <AuthorInput />
-          <Authors />
-        </div>
-        <div>
-          <h2>Book</h2>
-          <BookInput />
-          <Books />
-        </div>
-      </div>
-    );
-  }
-};
+	render() {
+		return (
+			<div className='App'>
+				<div>
+					<h2>Authors</h2>
+					<AuthorInput />
+					<Authors />
+				</div>
+				<div>
+					<h2>Book</h2>
+					<BookInput />
+					<Books />
+				</div>
+			</div>
+		);
+	}
+}
 
 export default App;
